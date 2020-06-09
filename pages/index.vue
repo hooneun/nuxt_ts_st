8bc7c4485e0fdcc94933241ad59689a5d7b55579
khon0316 <kimhoon0316@gmail.com>
2020-06-09 23:09:24 +0900

    vue ts config

--- pages/index.vue
+++ pages/index.vue
@@ -2,37 +2,29 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        hello-nuxt-ts
-      </h1>
-      <h2 class="subtitle">
-        My terrific Nuxt.js project
-      </h2>
+      <h1 class="title">hello-nuxt-ts</h1>
+      <h2 class="subtitle">My terrific Nuxt.js project</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
+        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
       </div>
     </div>
+    <h1 class="title">{{ name }}</h1>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Component, Vue } from 'nuxt-property-decorator'
 import Logo from '~/components/Logo.vue'
 
-export default Vue.extend({
+@Component({
   components: {
     Logo
   }
 })
+export default class IndexPage extends Vue {
+  name: string = 'hello-nuxt-ts-decorator'
+}
 </script>
 
 <style>
